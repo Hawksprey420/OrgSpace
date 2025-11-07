e9a21bb564633924e725651b93faec14cf3f60aa
Nestor Jann Asag <nestorjannasag@gmail.com>
2025-11-07 11:14:33 +0800

    refactored admin.py

--- orgSpace/orgSpace_app/admin.py
+++ orgSpace/orgSpace_app/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Program, StudentSubmission  
-# Register your models here.
+from .models import Program, StudentSubmission
+from django.utils import timezone
+
+@admin.action(description="Approve selected submissions")
+def approve_submissions(modeladmin, request, queryset):
+    queryset.update(
+        is_verified=True,
+        verified_by=request.user.username,
+        verified_at=timezone.now(),
+    )
+
+class StudentSubmissionAdmin(admin.ModelAdmin):
+    list_display = ("student_number", "lastname", "firstname", "program", "college", "is_verified")
+    list_filter = ("program", "college", "is_verified")
+    search_fields = ("student_number", "lastname", "firstname")
+    actions = [approve_submissions]
 
 admin.site.register(Program)
-admin.site.register(StudentSubmission)
+admin.site.register(StudentSubmission, StudentSubmissionAdmin)
