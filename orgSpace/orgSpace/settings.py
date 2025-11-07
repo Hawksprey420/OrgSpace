6d49184ae52d47baa88b0e8a87a51478880a728e
Nestor Jann Asag <nestorjannasag@gmail.com>
2025-11-07 15:47:05 +0800

    Code freeze, new site id for local dev, url configs and some boiler plate code for login

--- orgSpace/orgSpace/settings.py
+++ orgSpace/orgSpace/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
 ]
 
+SITE_ID = 1
+
 AUTHENTICATION_BACKENDS = [
 'django.contrib.auth.backends.ModelBackend',
 'allauth.account.auth_backends.AuthenticationBackend',
