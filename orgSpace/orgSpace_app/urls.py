d5712e64a266b5b79ca27d40e25aaa332cd34cb1
Nestor Jann Asag <nestorjannasag@gmail.com>
2025-11-09 00:23:02 +0800

    1st major refector for django end

--- orgSpace/orgSpace_app/urls.py
+++ orgSpace/orgSpace_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.landing_page, name="landing"),
-    path("officer-dashboard/", views.officer_dashboard, name="officer_dashboard"),
+    path("admin-dashboard/", views.admin_dashboard, name="officer_dashboard"),
     path("student-dashboard/", views.student_dashboard, name="student_dashboard"),
     path("login/", views.login_view, name="login"),
 ]
--- orgSpace/orgSpace_app/views.py
+++ orgSpace/orgSpace_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
 def landing_page(request):
-    """Landing page - accessible to everyone"""
     return render(request, "orgSpace_app/landing.html")
 
-def officer_dashboard(request):
-    return render(request, "orgSpace_app/officer_dashboard.html")
+
+def admin_dashboard(request):
+    return render(request, "orgSpace_app/admin/dashboard.html")
+
+
+def student_dashboard(request):
+    return render(request, "orgSpace_app/student/dashboard.html")
+
 
 def login_view(request):
     return render(request, "orgSpace_app/registration/login.html")
-
-def student_dashboard(request):
-    return render(request, "orgSpace_app/student_dashboard.html")
