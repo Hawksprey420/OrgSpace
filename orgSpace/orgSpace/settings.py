6c5f0b891ab7a4217c153010647d3a70dccb907f
Nestor Jann Asag <nestorjannasag@gmail.com>
2025-11-08 11:42:16 +0800

    Included static files and dashboard html files

--- orgSpace/orgSpace/settings.py
+++ orgSpace/orgSpace/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'orgSpace_app' / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- orgSpace/orgSpace_app/views.py
+++ orgSpace/orgSpace_app/views.py
@@ -2,5 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# def landing_page(request):
+#     return render(request, "orgSpace_app/landing.html")
+
 def landing_page(request):
+    """Landing page - accessible to everyone"""
     return render(request, "orgSpace_app/landing.html")
+
+#login_required
+# def home(request):
+#     """Dashboard/Home page - requires login"""
+#     return render(request, "orgSpace_app/home.html")
+
+#login_required
+def dashboard(request):
+    """Main officer dashboard - requires login"""
+    return render(request, "orgSpace_app/officer_dashboard.html")
+
+def dashboard(request):
+    """Main student dashboard - requires login"""
+    return render(request, "orgSpace_app/student_dashboard.html")
